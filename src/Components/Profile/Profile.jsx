b3f2c2e10fc883b1a8f0c6b9d018e251c525ab7b
Slav Mosoyan <vladanik1999@gmail.com>
2023-12-10 00:00:52 +0400

    initial commit

---
+++ src/Components/Profile/Profile.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import s from "./Profile.module.css";
+const Profile = () => {
+  return (
+    <div className={s.profile}>
+      <div>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6Sins9MyrQH_gpOpTdF5h12rMM-1_0JQ1vaXosGKe6YgYTtOO6KQmLbVToKCniw2JrSU&usqp=CAU"
+          alt=""
+        />
+      </div>
+      <div>ava + discrip</div>
+      <div>
+        posts
+        <div>new posts</div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
