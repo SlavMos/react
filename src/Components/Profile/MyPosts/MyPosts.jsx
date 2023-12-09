c5c27e8a3501acf606ea50f936a9cd6f2b48e9f9
Slav Mosoyan <vladanik1999@gmail.com>
2023-12-10 01:11:09 +0400

    add button

--- src/Components/Profile/MyPosts/MyPosts.jsx
+++ src/Components/Profile/MyPosts/MyPosts.jsx
@@ -5,6 +5,10 @@ const MyPost = () => {
   return (
     <div>
       posts
+      <div>
+        <textarea name="" id="" cols="30" rows="10"></textarea>
+        <button>Add post</button>
+      </div>
       <div>new posts</div>
     </div>
   );
