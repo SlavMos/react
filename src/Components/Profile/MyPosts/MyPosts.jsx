362541b32d708223e92222dd6b999bf8c1ca77e0
Slav Mosoyan <vladanik1999@gmail.com>
2023-12-10 01:16:03 +0400

    add Posts component

--- src/Components/Profile/MyPosts/MyPosts.jsx
+++ src/Components/Profile/MyPosts/MyPosts.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import s from "./MyPosts.module.css";
+import Posts from "./Posts/Posts";
 
 const MyPost = () => {
   return (
@@ -9,7 +10,10 @@ const MyPost = () => {
         <textarea name="" id="" cols="30" rows="10"></textarea>
         <button>Add post</button>
       </div>
-      <div>new posts</div>
+      <Posts />
+      <Posts />
+      <Posts />
+      <Posts />
     </div>
   );
 };
---
+++ src/Components/Profile/MyPosts/Posts/Posts.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import s from "./Posts.module.css";
+
+const Posts = () => {
+  return <div>new posts</div>;
+};
+
+export default Posts;
