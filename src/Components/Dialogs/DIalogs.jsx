b0b02dc59919b2d5af4d0feb1ff33c44f9e6e3dc
Slav Mosoyan <vladanik1999@gmail.com>
2024-01-06 21:59:24 +0400

    put the data aside

--- src/Components/Dialogs/DIalogs.jsx
+++ src/Components/Dialogs/DIalogs.jsx
@@ -15,21 +15,37 @@ const Message = (props) => {
 };
 
 const Dialogs = () => {
+  let dialogsData = [
+    { id: 1, name: "John" },
+    { id: 2, name: "Sam" },
+    { id: 3, name: "Nick" },
+    { id: 4, name: "Maluma" },
+    { id: 5, name: "Smith" },
+  ];
+
+  let messegesData = [
+    { id: 1, message: "HI" },
+    { id: 2, message: "Hello" },
+    { id: 3, message: "how are you?" },
+    { id: 4, message: "Good,and you?" },
+    { id: 5, message: "Good thanks" },
+  ];
+
   return (
     <div className={s.dialogs}>
       <div className={s.dialogsItems}>
-        <DialogItem name="John" id="1" />
-        <DialogItem name="Sam" id="2" />
-        <DialogItem name="Nick" id="3" />
-        <DialogItem name="Maluma" id="4" />
-        <DialogItem name="Smith" id="5" />
+        <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
+        <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
+        <DialogItem name={dialogsData[2].name} id={dialogsData[2].id} />
+        <DialogItem name={dialogsData[3].name} id={dialogsData[3].id} />
+        <DialogItem name={dialogsData[4].name} id={dialogsData[4].id} />
       </div>
       <div className={s.messages}>
-        <Message message="HI" />
-        <Message message="Hello" />
-        <Message message="how are you?" />
-        <Message message="Good,and you?" />
-        <Message message="Good thanks" />
+        <Message message={messegesData[0].message} />
+        <Message message={messegesData[1].message} />
+        <Message message={messegesData[2].message} />
+        <Message message={messegesData[3].message} />
+        <Message message={messegesData[4].message} />
       </div>
     </div>
   );
