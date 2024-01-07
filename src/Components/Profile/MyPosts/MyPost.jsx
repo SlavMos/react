02f98310ef8acd94587222188738e29dc544cd80
Slav Mosoyan <vladanik1999@gmail.com>
2024-01-07 19:03:51 +0400

    add postData

--- src/Components/Profile/MyPosts/MyPost.jsx
+++ src/Components/Profile/MyPosts/MyPost.jsx
@@ -3,6 +3,12 @@ import s from "./MyPost.module.css";
 import Post from "./Posts/Post";
 
 const MyPost = () => {
+  let posData = [
+    { id: 0, messages: "Hello how are you", likesCount: 1 },
+
+    { id: 1, messages: "Hyyyy,okay", likesCount: 15 },
+  ];
+
   return (
     <div className={s.MyPost}>
       posts
@@ -10,8 +16,8 @@ const MyPost = () => {
         <textarea name="" id="" cols="15" rows="10"></textarea>
         <button>Add post</button>
       </div>
-      <Post messages="Hello how are you" likesCount="0" />
-      <Post messages="Hello are you" likesCount="10" />
+      <Post messages={posData[0].messages} likesCount={posData[0].likesCount} />
+      <Post messages={posData[1].messages} likesCount={posData[1].likesCount} />
     </div>
   );
 };
