cb0b1c2ba2df2e21cf32cdadf0111b2636095bef
Slav Mosoyan <vladanik1999@gmail.com>
2024-01-09 20:11:00 +0400

    Message component

--- src/Components/Dialogs/DIalogs.jsx
+++ src/Components/Dialogs/DIalogs.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 import s from "./Dialogs.module.css";
-import { NavLink } from "react-router-dom";
 import DialogItem from "./DialogItem/DialogItem";
-
-const Message = (props) => {
-  return <div className={s.message}>{props.message}</div>;
-};
+import Message from "./Message/Message";
 
 const Dialogs = () => {
   let dialogsData = [
