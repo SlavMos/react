e5974b6bcd943b3977d36fd0b45951c5397903a5
Slav Mosoyan <vladanik1999@gmail.com>
2024-01-12 21:35:30 +0400

    onClick, ref, VirtualDOM

--- src/App.js
+++ src/App.js
@@ -8,31 +8,29 @@ import Dialogs from "./Components/Dialogs/DIalogs";
 import News from "./Components/News/News";
 const App = (props) => {
   return (
-    <BrowserRouter>
-      <div className="app-wriper">
-        <Header />
-        <Nav />
-        <div className="app-wriper-content">
-          <Routes>
-            {" "}
-            {/* Роуты используем для загрузки новой страницы (ет переключаться по ссылкам)*/}
-            <Route
-              path="/profile"
-              element={<Profile state={props.state.profilePage} />}
-            />
-            <Route
-              path="/"
-              element={<Profile state={props.state.profilePage} />}
-            />
-            <Route
-              path="/dialogs/*"
-              element={<Dialogs state={props.state.messagesPage} />}
-            />
-            <Route path="/news" element={<News />} />
-          </Routes>
-        </div>
+    <div className="app-wriper">
+      <Header />
+      <Nav />
+      <div className="app-wriper-content">
+        <Routes>
+          {" "}
+          {/* Роуты используем для загрузки новой страницы (ет переключаться по ссылкам)*/}
+          <Route
+            path="/profile"
+            element={<Profile state={props.state.profilePage} />}
+          />
+          <Route
+            path="/"
+            element={<Profile state={props.state.profilePage} />}
+          />
+          <Route
+            path="/dialogs/*"
+            element={<Dialogs state={props.state.messagesPage} />}
+          />
+          <Route path="/news" element={<News />} />
+        </Routes>
       </div>
-    </BrowserRouter>
+    </div>
   );
 };
 export default App;
--- src/Components/Profile/MyPosts/MyPost.jsx
+++ src/Components/Profile/MyPosts/MyPost.jsx
@@ -4,14 +4,29 @@ import Post from "./Posts/Post";
 
 const MyPost = (props) => {
   let postsElement = props.posts.map((p) => (
-    <Post messages={p.messages} likesCount={p.likesCount} />
+    <Post messages={p.messages} likesCount={p.likesCount} /> // при помощи map сосдаем новый массив который будет все элементы из старого(для того чтобы много раз не добавлять компонету)
   ));
+
+  let newPostElement = React.createRef(); //создаем ссылку
+
+  let addPost = () => {
+    //создаем функцию которую поставим на баттон(onclick) что бы она выполнилась при нажатии
+    let text = newPostElement.current.value; // ПОКАЗЫВАЕТ ЗНАЧЕНИЕ ТЕКСТАРЕА
+    alert(text);
+  };
+
   return (
     <div className={s.MyPost}>
       posts
       <div>
-        <textarea name="" id="" cols="15" rows="10"></textarea>
-        <button>Add post</button>
+        <textarea ref={newPostElement}></textarea>
+        {/* добавили ссылку на
+        textarea что бы могли обратиться к нему */}
+        <button
+          onClick={addPost} //onclick позволяет кликать по кнопкеfd ADDPOST Это функция которую добавили к кнопке
+        >
+          Add post
+        </button>
       </div>
       {postsElement}
     </div>
